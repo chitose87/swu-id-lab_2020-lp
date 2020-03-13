6687df689179352e440ba550acf93120aa58c127
日野みなみ <hinominami21@gmail.com>
2020-03-14 01:00:04 +0900

    こんばんは。作ってみた。

--- pages/hinomi.vue
+++ pages/hinomi.vue
@@ -2,30 +2,30 @@
   <div>
     <header>
       <div class="container">
-        <h1 class="block">ヤバイホームページ屋さん</h1>
+        <h2 class="block">ヤバイホームページ屋さん</h2>
         <div class="hoge">
-          <a href="#" class="hedderlink">お知らせ</a>
-          <a href="#" class="hedderlink">企業情報</a>
-          <a href="#" class="hedderlink">事業内容</a>
-          <a href="#" class="hedderlink">採用情報</a>
-          <a href="#" class="hedderlink">お問い合わせ</a>
+          <a href="#" class="hedder-link">お知らせ</a>
+          <a href="#" class="hedder-link">企業情報</a>
+          <a href="#" class="hedder-link">事業内容</a>
+          <a href="#" class="hedder-link">採用情報</a>
+          <a href="#" class="hedder-link">お問い合わせ</a>
         </div>
       </div>
     </header>
 
     <div class="img">
-      <img src="https://placehold.jp/250x150.png"/>
+      <img src="https://placehold.jp/250x150.png" />
     </div>
 
-    <div class="container">
-      <div class="container1">
+    <section class="news-section">
+      <div class="container">
         <div class="section">
-          <h1 class="title">お知らせ</h1>
+          <h2>お知らせ</h2>
           <div class="info">
             <a href="#">2019/04/01 プレスリリース 新サービス発表会について</a>
-            <br/>
+            <br />
             <a href="#">2019/04/01 メディア掲載 〇〇新聞に掲載されました</a>
-            <br/>
+            <br />
             <a href="#">2019/04/01 採用情報 2020年度新卒採用を開始いたします</a>
           </div>
           <div class="information">
@@ -33,123 +33,121 @@
           </div>
         </div>
       </div>
-    </div>
+    </section>
 
-    <div>
+    <section class="link-section">
       <div class="container">
-        <div class="container2">
-          <div class="nav">
-            <img src="https://placehold.jp/150x150.png"/>
-            <a href="#">エンジニア急募</a>
-          </div>
-          <div class="nav">
-            <img src="https://placehold.jp/150x150.png"/>
-            <a href="#">
-              ヤバイ会計ソフト
-              <br/>「YABEE」
-            </a>
-          </div>
-          <div class="nav">
-            <img src="https://placehold.jp/150x150.png"/>
-            <a href="#">
-              広報ブログは
-              <br/>こちら
-            </a>
-          </div>
+        <div class="nav">
+          <img src="https://placehold.jp/150x150.png" />
+          <a href="#">エンジニア急募</a>
+        </div>
+        <div class="nav">
+          <img src="https://placehold.jp/150x150.png" />
+          <a href="#">
+            ヤバイ会計ソフト
+            <br />「YABEE」
+          </a>
+        </div>
+        <div class="nav">
+          <img src="https://placehold.jp/150x150.png" />
+          <a href="#">
+            広報ブログは
+            <br />こちら
+          </a>
         </div>
       </div>
-    </div>
+    </section>
 
-    <div class="container">
-      <div class="container3">
+    <footer class="footer">
+      <div class="container">
         <div class="hudder">
           <div class="img2">
-            <img src="https://placehold.jp/150x150.png"/>
-            <img src="https://placehold.jp/150x150.png"/>
+            <img src="https://placehold.jp/150x150.png" />
+            <img src="https://placehold.jp/150x150.png" />
           </div>
           <div class="info2">
-            <a href="#" class="hedderlink">お知らせ</a>
-            <a href="#" class="hedderlink">企業情報</a>
-            <a href="#" class="hedderlink">事業内容</a>
-            <a href="#" class="hedderlink">採用情報</a>
-            <a href="#" class="hedderlink">お問い合わせ</a>
+            <a href="#" class="footer-link">お知らせ</a>
+            <a href="#" class="footer-link">企業情報</a>
+            <a href="#" class="footer-link">事業内容</a>
+            <a href="#" class="footer-link">採用情報</a>
+            <a href="#" class="footer-link">お問い合わせ</a>
           </div>
         </div>
         <small class="small">©︎2019 ヤバイホームページ屋さん</small>
       </div>
-    </div>
+    </footer>
   </div>
 </template>
 
 <script lang="ts">
-  import {Component} from "~/node_modules/vue-property-decorator";
-  import {Vue} from "~/node_modules/nuxt-property-decorator";
+import { Component } from "~/node_modules/vue-property-decorator";
+import { Vue } from "~/node_modules/nuxt-property-decorator";
 
-  @Component({
-    components: {}
-  })
-  export default class TopPageComp extends Vue {
-  }
+@Component({
+  components: {}
+})
+export default class TopPageComp extends Vue {}
 </script>
 
 <style lang="scss">
+.container {
+  padding-top: 15pt;
+  padding-bottom: 15pt;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+header {
   .container {
-    padding-top: 15pt;
-    padding-bottom: 15pt;
-    margin-left: auto;
-    margin-right: auto;
+    display: flex;
+    justify-content: space-between;
   }
 
-  header {
-    .container {
-      display: flex;
-      justify-content: space-between;
-    }
-
-    h1 {
-      color: black;
-      font-size: 16px;
-    }
-
-    .block {
-      display: flex;
-      justify-content: start;
-    }
-
-    .hoge {
-      /*padding-left: 610px;*/
-      display: flex;
-      justify-content: space-between;
-    }
-
-    .hedderlink {
-      color: blue;
-      font-size: 11px;
-      text-decoration-line: none;
-    }
+  h2 {
+    color: black;
   }
 
-  .img {
-    img {
-      width: 100%;
-      height: auto;
-    }
+  .block {
+    display: flex;
+    justify-content: start;
   }
 
-  .container1 {
-    padding-top: 30px;
-    padding-bottom: 30px;
+  .hoge {
+    /*padding-left: 610px;*/
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .hedder-link {
+    color: blue;
+    font-size: 1.1em;
+    text-decoration-line: none;
+    padding: 10px;
+  }
+}
+
+.img {
+  img {
+    width: 100%;
+    height: 400px;
+  }
+}
+
+.news-section {
+  padding-top: 30px;
+  padding-bottom: 30px;
+
+  .container {
     border-style: solid;
-    border-color: grey;
+    border-color: gray;
   }
-
   .section {
     width: auto;
     height: auto;
     padding: 10px;
   }
 
-  .title {
+  h2 {
     text-align: center;
   }
 
@@ -175,8 +173,10 @@
       text-decoration: none;
     }
   }
+}
 
-  .container2 {
+.link-section {
+  .container {
     display: flex;
     justify-content: space-between;
   }
@@ -189,7 +189,7 @@
     white-space: nowrap;
     text-decoration: none;
     position: absolute;
-    color: black;
+    color: rgb(53, 52, 52);
     top: 50%;
     left: 50%;
     -ms-transform: translate(-50%, -50%);
@@ -205,49 +205,48 @@
     width: 320px;
     height: 150px;
   }
+}
 
-  .container3 {
-    border-top: 1px solid gray;
-  }
-
+.footer {
+  border-top: 1px solid gray;
   .hudder {
     display: flex;
-  }
-
-  .img2 {
-    display: flex;
-    width: 50px;
-    height: 50px;
-    padding-right: 10px;
-
-    img {
-      padding-top: 10px;
-      padding-bottom: 10px;
+    justify-content: space-between;
+    .img2 {
+      display: flex;
+      width: 50px;
+      height: 50px;
       padding-right: 10px;
-      width: 100%;
-      height: 100%;
+
+      img {
+        padding-top: 10px;
+        padding-bottom: 10px;
+        padding-right: 10px;
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    .info2 {
+      font-size: 1.1em;
+      padding-bottom: 10px;
+      display: flex;
+      justify-content: space-between;
+      .footer-link {
+        color: blue;
+        text-decoration: none;
+        padding: 10px;
+      }
     }
   }
 
-  .info2 {
-    padding-top: 20px;
-    padding-bottom: 20px;
-    padding-left: 690px;
-    display: flex;
-    justify-content: space-between;
-  }
-
-  .hedderlink {
-    color: blue;
-    text-decoration: none;
-  }
-
   .small {
+    display: block;
     color: gray;
     padding-top: 10px;
-    padding-left: 400px;
-    padding-right: 400px;
+    text-align: center;
     margin-left: auto;
     margin-right: auto;
   }
+}
 </style>
