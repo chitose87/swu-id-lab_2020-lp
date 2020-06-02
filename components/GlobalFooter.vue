959baa96ee3a2509b8118442be0ffe7ec1c90873
chitose87 <chitose87@gmail.com>
2020-06-02 13:26:41 +0900

    add component

---
+++ components/GlobalFooter.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  footer.global-footer
+
+
+</template>
+
+<script lang="ts">
+  import {Component} from "~/node_modules/vue-property-decorator";
+  import {Vue} from "~/node_modules/nuxt-property-decorator";
+
+  @Component({
+    components: {}
+  })
+  export default class GlobalFooter extends Vue {
+  }
+</script>
+
+<style lang="scss">
+  .global-footer{
+    
+  }
+</style>
---
+++ components/GlobalHeader.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  header.global-header
+
+
+</template>
+
+<script lang="ts">
+  import {Component} from "~/node_modules/vue-property-decorator";
+  import {Vue} from "~/node_modules/nuxt-property-decorator";
+
+  @Component({
+    components: {}
+  })
+  export default class GlobalHeader extends Vue {
+  }
+</script>
+
+<style lang="scss">
+  .global-header{
+
+  }
+</style>
