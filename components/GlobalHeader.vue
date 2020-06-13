7492a086e610214432c636f52f9f31cae2e9d1f0
chitose87 <chitose87@gmail.com>
2020-06-14 00:49:05 +0900

    memberの追加

--- components/GlobalHeader.vue
+++ components/GlobalHeader.vue
@@ -1,52 +1,64 @@
 <template lang="pug">
   header.global-header
-    .container
+    .global-header__body.container-fluid
       img.logo(src="/imgs/logo_only.png" alt="logo")
-      p About
-      p PROJECTS
-      p JoinUs.
-      p DP
-      p |Members
+      a.link-item(href="/#about") About
+      a.link-item(href="/#project") Projects
+      a.link-item(href="/#joinus") JoinUs.
+      a.link-item(href="/#dp") DP
+      span.link-item |
+      a.link-item(href="/member/") Members
 
 </template>
 
 <script lang="ts">
-import { Component } from "~/node_modules/vue-property-decorator";
-import { Vue } from "~/node_modules/nuxt-property-decorator";
+  import {Component} from "~/node_modules/vue-property-decorator";
+  import {Vue} from "~/node_modules/nuxt-property-decorator";
 
-@Component({
-  components: {}
-})
-export default class GlobalHeader extends Vue {}
+  @Component({
+    components: {}
+  })
+  export default class GlobalHeader extends Vue {
+  }
 </script>
 
 <style lang="scss">
-.global-header {
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: $zindex-on;
+  .global-header {
+    /*position: fixed;*/
+    position: sticky;
+    top: 0;
+    left: 0;
+    z-index: $zindex-on;
 
-  width: 100%;
-  height: 3rem;
-  background-color: white;
-  .container {
-    height: 48px;
-    display: flex;
-    padding-top: 5px;
-    padding-left: 8px;
-    padding-right: 0px;
-    p {
-      margin: 7px;
+    width: 100%;
+    height: 3rem;
+    background-color: white;
 
-      color: #707070;
-    }
-    img {
-      margin: 7px;
-      width: 26px;
-      height: 23px;
-      padding-right: 5px;
+    box-shadow: $shadow-text-black;
+
+    &__body {
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .logo {
+        margin-right: auto;
+      }
+
+      .link-item {
+        margin-left: 0.75rem;
+        color: #707070;
+        text-decoration: none;
+        font-size: 15px;
+      }
+
+      img {
+        margin: 7px;
+        width: 26px;
+        height: 23px;
+        padding-right: 5px;
+      }
     }
   }
-}
 </style>
---
+++ pages/member.vue
@@ -0,0 +1,125 @@
+<template lang="pug">
+  div
+    GlobalHeader
+    main
+      .container
+        .member(v-for="items in spreadSheetData")
+          p.member__item.name
+            span(v-html="items[0]")
+          p.member__item.nick-name
+            span ニックネーム：
+            span(v-html="items[1]")
+          p.member__item.group
+            span(v-html="items[2]")
+          p.member__item.unit
+            span Unit：
+            span(v-html="items[3]")
+          p.member__item.like
+            span 好きなモノ：
+            span(v-html="items[4]")
+          p.member__item.bad
+            span 嫌いなモノ：
+            span(v-html="items[5]")
+          p.member__item.will
+            span やりたいこと：
+            span(v-html="items[6]")
+          p.member__item.face(v-if="items[7]")
+            img(:src="getPath(items[7])")
+
+    GlobalFooter
+</template>
+
+<script lang="ts">
+  import {Component} from "~/node_modules/vue-property-decorator";
+  import {Vue} from "~/node_modules/nuxt-property-decorator";
+  import GlobalHeader from "~/components/GlobalHeader.vue";
+  import GlobalFooter from "~/components/GlobalFooter.vue";
+
+  @Component({
+    components: {
+      GlobalFooter,
+      GlobalHeader,
+    }
+  })
+  export default class MemberPageComp extends Vue {
+
+    spreadSheetData: string[][] = [];
+
+    head() {
+      return {
+        // title: 'My Site',
+        script: [
+          {src: 'https://apis.google.com/js/api.js'},
+        ]
+      }
+    }
+
+    getPath(url: string) {
+      // url.match(/\/d\/([^\/]+)/)[1]
+      // console.log(url, url.match(/\/d\/([^\/]+)/))
+      return `https://drive.google.com/uc?id=${url.match(/\/d\/([^\/]+)/)![1]}`;
+    }
+
+    created() {
+      try {
+        gapi.load('client:auth2', () => {
+          gapi.client.init({
+            apiKey: "AIzaSyAO4hoO627ZHxBqjz4Uet7M5kscJLe8j-M",
+            clientId: "90996999377-5ipd5pojr42ahatuhefgcmc7ijs4rtph.apps.googleusercontent.com",
+            discoveryDocs: [
+              "https://sheets.googleapis.com/$discovery/rest?version=v4"
+            ],
+            scope: "https://www.googleapis.com/auth/spreadsheets.readonly"
+          }).then(() => {
+            gapi.client.sheets.spreadsheets.values.get({
+              spreadsheetId: "1WQBhlnMZJO1OwpU_VkXz2_24Uj4Aex7O3gpOCKwaZzA",
+              range: "profile",
+              valueRenderOption: 'UNFORMATTED_VALUE',
+              // dateTimeRenderOption: 'SERIAL_NUMBER',
+              dateTimeRenderOption: 'FORMATTED_STRING',
+            }).then((response) => {
+              console.log(response.result);
+
+              response.result.values!.shift();
+              let array = <any[]>response.result.values;
+              for (let i = array.length - 1; i > 0; i--) {
+                let r = Math.floor(Math.random() * (i + 1));
+                let tmp = array[i];
+                array[i] = array[r];
+                array[r] = tmp;
+              }
+              array.forEach((v) => {
+                this.spreadSheetData.push(v);
+              });
+            }, (e: Error) => {
+              console.log(e);
+            });
+          }, (error: any) => {
+            console.log(error);
+          });
+        });
+      } catch (e) {
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .member {
+    border: 1px solid $color-border;
+    padding: 1rem;
+    margin-bottom: 3rem;
+
+    &__item {
+      margin-bottom: 0;
+    }
+
+    .face {
+      img {
+        width: 100%;
+        height: auto;
+      }
+    }
+  }
+</style>
